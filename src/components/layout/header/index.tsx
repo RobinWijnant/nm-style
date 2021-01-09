214fde9a46514167be6c326ac3acc261c32c30fa
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2021-01-09 21:38:10 +0100

    make header responsive

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Button from "../../Button";
 import { graphql, Link, useStaticQuery } from "gatsby";
 import logo from "../../../assets/logo.jpg";
 import styles from "./index.module.css";
+import clsx from "clsx";
 
 type QueryProps = {
   site: {
@@ -28,32 +29,46 @@ const navItemsQuery = graphql`
   }
 `;
 
-const Header = () => {
+const Header: React.FC = () => {
   const data = useStaticQuery<QueryProps>(navItemsQuery);
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const navItems = data.site.siteMetadata.navItems;
 
+  useEffect(() => {
+    document.body.style.overflow = isMenuOpen ? "hidden" : "initial";
+  }, [isMenuOpen]);
+
+  const headerClassNames = clsx(styles.header, {
+    [styles.isMobileMenuOpen]: isMenuOpen,
+  });
+
   return (
-    <header className={styles.header}>
+    <header className={headerClassNames}>
       <img className={styles.logo} src={logo} alt="NM-Style logo" />
 
-      <nav className={styles.right}>
-        <div className={styles.contact}>
-          <span className={styles.phone}>0470 00 00 00</span>
-          <Button to="/online-afspraak-maken">Online afspraak</Button>
-        </div>
-        <ul className={styles.nav}>
-          {navItems.map((item) => (
-            <Link
-              className={styles.navItem}
-              activeClassName={styles.active}
-              key={item.title}
-              to={item.href}
-            >
-              <li>{item.title}</li>
-            </Link>
-          ))}
-        </ul>
+      <div className={styles.contact}>
+        <span className={styles.phone}>0470 00 00 00</span>
+        <Button to="/online-afspraak-maken">Online afspraak</Button>
+      </div>
+
+      <nav className={styles.nav}>
+        {navItems.map((item) => (
+          <Link
+            className={styles.navItem}
+            activeClassName={styles.active}
+            key={item.title}
+            to={item.href}
+          >
+            {item.title}
+          </Link>
+        ))}
       </nav>
+
+      <div
+        role="button"
+        className={styles.mobileMenuButton}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      />
     </header>
   );
 };
