7ae8a5fc65322f2dcdd9eb1ca99e4c9bcb8ba1ff
Robin Wijnant <robin.wijnant@inthepocket.mobi>
2021-01-23 13:50:40 +0100

    add proper animations to nm style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { Link } from "gatsby";
 import React from "react";
 import styles from "./index.module.css";
@@ -5,10 +6,11 @@ import styles from "./index.module.css";
 type Props = {
   to: string;
   onClick: () => void;
+  className?: string;
 };
 
-const Button: React.FC<Props> = ({ children, to, onClick }) => (
-  <span className={styles.button}>
+const Button: React.FC<Props> = ({ children, to, onClick, className }) => (
+  <span className={clsx(styles.button, className)}>
     {children}
     <Link to={to} className={styles.text} onClick={onClick}>
       {children}
--- src/components/RebrandingModal/index.tsx
+++ src/components/RebrandingModal/index.tsx
@@ -4,7 +4,6 @@ import Image, { FixedObject } from "gatsby-image";
 import React from "react";
 import Modal from "react-modal";
 import styles from "./index.module.css";
-import { Fade } from "react-awesome-reveal";
 
 type Props = {
   isOpen: boolean;
@@ -56,32 +55,29 @@ const RebrandingModal: React.FC<Props> = ({ isOpen, onRequestClose }) => {
       className={styles.modal}
     >
       <div className={styles.content}>
-        <div className={styles.brands}>
-          <div className={styles.brand}>
-            <Fade direction="left" triggerOnce>
-              <Image fixed={bennaLogo} alt="benna logo" />
-            </Fade>
-            <Fade triggerOnce delay={500}>
-              <span className={styles.url}>benna.be</span>
-            </Fade>
-          </div>
-          <Fade direction="up" triggerOnce delay={1500}>
-            <span className={styles.becomes}>verandert in</span>
-          </Fade>
-          <div className={styles.brand}>
-            <Fade direction="right" triggerOnce delay={2500}>
-              <Image fixed={nmStyleLogo} alt="nm-style logo" />
-            </Fade>
-            <Fade triggerOnce delay={3000}>
-              <span className={styles.url}>nm-style.be</span>
-            </Fade>
-          </div>
-        </div>
-        <Fade triggerOnce delay={4000}>
-          <Button to="/" onClick={() => onRequestClose()}>
-            Naar nm-style.be
-          </Button>
-        </Fade>
+        <Image
+          className={styles.bennaLogo}
+          fixed={bennaLogo}
+          alt="benna logo"
+        />
+        <span className={styles.disappears}>verdwijnt</span>
+        <span className={styles.becomes}>en verandert in</span>
+        <Image
+          className={styles.nmStyleLogo}
+          fixed={nmStyleLogo}
+          alt="nm-style logo"
+        />
+        <Button
+          to="/"
+          onClick={() => onRequestClose()}
+          className={styles.button}
+        >
+          Naar nm-style.be
+        </Button>
+        <span className={styles.note}>
+          Deze doorverwijzing van benna.be naar nm-style.be zal niet meer werken
+          vanaf 22-10-2021
+        </span>
       </div>
     </Modal>
   );
